95e22d2878e868d20c88ab0935e254633adff6b7
GuySizemore <guysizemore@me.com>
2017-03-28 19:45:12 -0400

    updated jQuery added media queries

--- js/app.js
+++ js/app.js
@@ -1,48 +1,24 @@
+$(function () {
+  var $overlay = $('<div id="overlay"></div>');
+  var $image = $("<img>");
+  var $caption =$("<p></p>");
 
- /*
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
+  $overlay.append($image); //add image to overlay
+  $overlay.append($caption);
+  $("body").append($overlay); //add overlay to body
 
-$overlay.append($image);
-$overlay.append($caption);
-$("body").append($overlay);
+  $(".main-gallery a").click(function(event){
+    event.preventDefault();
+    var imageLocation = $(this).attr("href");
 
-$("#main-gallery a").click(function(event){
-  event.preventDefault();
-  var imageLocation = $(this).attr("href");
-
-  $image.attr("src", imageLocation);
-  $.overlay.show();
-
-var captionText = $(this).children("img").attr("alt");
-  $caption.text(captionText);
-});
-
-$overlay.click(function(){
-  $overlay.hide();
-});
-*/
-
-
-/*Check to see if jQuery is loaded
-<script>
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
-</script>*/
-
-
-<script>
-$(document).ready(function(){
-  $("p").dblclick.(function(){
-    $(this).hide();
+    $image.attr("src", imageLocation); //update overlay with img
+    $overlay.show();
+  var captionText = $(this).children("img").attr("alt");
+    $caption.text(captionText); //add caption to img
   });
-</script>
+
+  $overlay.click(function() {
+    $overlay.hide();
+  });
+
 });
